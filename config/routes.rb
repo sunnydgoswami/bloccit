82ef446472c9c8098d7b74e363c4c49d3e268f32
SunnyGoswami <sunnydgoswami@gmail.com>
2017-08-07 20:06:01 +0000

    checkpoint18 completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-
-  get 'welcome/about'
   
-  get 'welcome/contact'
+  resources:posts
+
+  get 'about' => 'welcome#about'
 
   root 'welcome#index'
   
