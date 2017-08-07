eebdd33c428024410a55daaacb859eb2181e5fb1
SunnyGoswami <sunnydgoswami@gmail.com>
2017-08-07 21:59:24 +0000

    Checkpoint 19 completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,9 +4,25 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
+  end
+  
+  def create
+    @post = Post.new
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+    
+    if @post.save
+      flash[:notie] = "Post was saved."
+      redirect_to @post
+    else
+      flash.now[:alert] = "There waas an error saving the post. Please try again."
+      render :new
+    end
   end
 
   def edit
