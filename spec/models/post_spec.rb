244c0e07e9972a80dfec3dde38fba5a034143698
SunnyGoswami <sunnydgoswami@gmail.com>
2017-08-10 16:30:06 +0000

    checkpoint 22 completed

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -11,6 +11,13 @@
    let(:post) { topic.posts.create!(title: title, body: body) }
  
    it { is_expected.to belong_to(:topic) }
+   
+   it { is_expected.to validate_presence_of(:title) }
+   it { is_expected.to validate_presence_of(:body) }
+   it { is_expected.to validate_presence_of(:topic) }
+   
+   it { is_expected.to validate_length_of(:title).is_at_least(5) }
+   it { is_expected.to validate_length_of(:body).is_at_least(20) }
 
    describe "attributes" do
      it "has a title and body attribute" do
