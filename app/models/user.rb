fdf58ccea2a2bc303679ad8b40b45ff8c516c2a4
SunnyGoswami <sunnydgoswami@gmail.com>
2017-08-14 18:57:19 +0000

    checkpoint 27 completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,20 @@
- class User < ActiveRecord::Base
-   before_save { self.email = email.downcase if email.present? }
+class User < ActiveRecord::Base
+  has_many :posts
 
-   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
-   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
-   validates :password, length: { minimum: 6 }, allow_blank: true
-   validates :email,
-             presence: true,
-             uniqueness: { case_sensitive: false },
-             length: { minimum: 3, maximum: 254 }
+  before_save { self.email = email.downcase }
+  before_save { self.name = self.name.split.map!{|n| n.capitalize}.join(' ') if self.name }
 
-   has_secure_password
- end
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  NAME_REGEX = /\A[A-Z]\w+(\s[A-Z]\w+)?\z/
+
+  validates :name, length: { minimum: 1, maximum: 100 }, format: { with: NAME_REGEX }, presence: true
+  validates :password, presence: true, length: { minimum: 6 }, if: 'password_digest.nil?'
+  validates :password, length: { minimum: 6 }, allow_blank: true
+  validates :email,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { minimum: 3, maximum: 100 },
+            format: { with: EMAIL_REGEX }
+
+  has_secure_password
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,71 +1,58 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-   let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "password") }
-   # Shoulda tests for name
-   it { is_expected.to validate_presence_of(:name) }
-   it { is_expected.to validate_length_of(:name).is_at_least(1) }
- 
-   # Shoulda tests for email
-   it { is_expected.to validate_presence_of(:email) }
-   it { is_expected.to validate_uniqueness_of(:email) }
-   it { is_expected.to validate_length_of(:email).is_at_least(3) }
-   it { is_expected.to allow_value("user@bloccit.com").for(:email) }
- 
-   # Shoulda tests for password
-   it { is_expected.to validate_presence_of(:password) }
-   it { is_expected.to have_secure_password }
-   it { is_expected.to validate_length_of(:password).is_at_least(6) }
- 
-   describe "attributes" do
-     it "should have name and email attributes" do
-       expect(user).to have_attributes(name: "Bloccit User", email: "user@bloccit.com")
-     end
-   end
-   
-   describe "invalid user" do
-     let(:user_with_invalid_name) { User.new(name: "", email: "user@bloccit.com") }
-     let(:user_with_invalid_email) { User.new(name: "Bloccit User", email: "") }
- 
-     it "should be an invalid user due to blank name" do
-       expect(user_with_invalid_name).to_not be_valid
-     end
- 
-     it "should be an invalid user due to blank email" do
-       expect(user_with_invalid_email).to_not be_valid
-     end
-   end
-   
-   describe "POST create" do
-     it "returns an http redirect" do
-       post :create, user: new_user_attributes
-       expect(response).to have_http_status(:redirect)
-     end
- 
-     it "creates a new user" do
-       expect{
-         post :create, user: new_user_attributes
-       }.to change(User, :count).by(1)
-     end
- 
-     it "sets user name properly" do
-       post :create, user: new_user_attributes
-       expect(assigns(:user).name).to eq new_user_attributes[:name]
-     end
- 
-     it "sets user email properly" do
-       post :create, user: new_user_attributes
-       expect(assigns(:user).email).to eq new_user_attributes[:email]
-     end
- 
-     it "sets user password properly" do
-       post :create, user: new_user_attributes
-       expect(assigns(:user).password).to eq new_user_attributes[:password]
-     end
- 
-     it "sets user password_confirmation properly" do
-       post :create, user: new_user_attributes
-       expect(assigns(:user).password_confirmation).to eq new_user_attributes[:password_confirmation]
-     end
-   end   
+  let(:user) { User.create! name: 'Bloccit User', email: 'user@bloccit.com', password: 'password' }
+
+  it { should have_many(:posts) }
+
+  # tests for name
+  it { should validate_presence_of(:name) }
+  it { should validate_length_of(:name).is_at_least(1) }
+  it { should allow_value('Bloccit User').for(:name) }
+  it { should_not allow_value('bloccit user').for(:name) }
+
+  # tests for email
+  it { should validate_presence_of(:email) }
+  it { should validate_uniqueness_of(:email) }
+  it { should validate_length_of(:email).is_at_least(3) }
+  it { should allow_value('user@bloccit.com').for(:email) }
+  it { should_not allow_value('userbloccit.com').for(:email) }
+
+  # tests for password
+  it { should validate_presence_of(:password) }
+  it { should have_secure_password }
+  it { should validate_length_of(:password).is_at_least(6) }
+
+  describe 'attributes' do
+    it 'should respond to name' do
+      expect(user).to respond_to(:name)
+    end
+
+    it 'should respond to email' do
+      expect(user).to respond_to(:email)
+    end
+  end
+
+  describe 'invalid user' do
+    let(:user_with_invalid_name) { User.new name: '', email: 'user@bloccit.com', password: 'password' }
+    let(:user_with_invalid_name_format) { User.new name: 'bloccit user', email: 'user@bloccit.com', password: 'password' }
+    let(:user_with_invalid_email) { User.new name: 'Bloccit User', email: '', password: 'password' }
+    let(:user_with_invalid_email_format) { User.new name: 'Bloccit User', email: 'userbloccit.com', password: 'password' }
+
+    it 'should be an invalid user due to blank name' do
+      expect(user_with_invalid_name).to_not be_valid
+    end
+
+    it 'should be an invalid user due to incorrectly formatted name' do
+      expect(user_with_invalid_name_format).to_not be_valid
+    end
+
+    it 'should be an invalid user due to blank email' do
+      expect(user_with_invalid_email).to_not be_valid
+    end
+
+    it 'should be an invalid user due to incorrectly formatted email address' do
+      expect(user_with_invalid_email_format).to_not be_valid
+    end
+  end
 end
