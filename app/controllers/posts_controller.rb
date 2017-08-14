b0e80476cfbd7fc21abd7bce62f5d35394d424da
SunnyGoswami <sunnydgoswami@gmail.com>
2017-08-14 19:40:21 +0000

    commit something post controller got updated somehow

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,4 @@
 class PostsController < ApplicationController
-  before_action :require_sign_in, except: :show
 
   def show
     @post = Post.find(params[:id])
@@ -9,18 +8,19 @@ class PostsController < ApplicationController
     @topic = Topic.find(params[:topic_id])
     @post = Post.new
   end
-
+  
   def create
+    @post = Post.new
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
     @topic = Topic.find(params[:topic_id])
-    @post = @topic.posts.build(post_params)
-    @post.user = current_user
-
-
+    @post.topic = @topic
+    
     if @post.save
-      flash[:notice] = "Post was saved."
+      flash[:notie] = "Post was saved."
       redirect_to [@topic, @post]
     else
-      flash[:error] = "There was an error saving the post. Please try again"
+      flash.now[:alert] = "There waas an error saving the post. Please try again."
       render :new
     end
   end
@@ -28,35 +28,34 @@ class PostsController < ApplicationController
   def edit
     @post = Post.find(params[:id])
   end
-
-  def update
-    @post = Post.find(params[:id])
-    @post.assign_attributes(post_params)
-
-    if @post.save
-      flash[:notice] = "Post was updated."
-      redirect_to [@post.topic, @post]
-    else
-      flash[:error] = "There was an error saving the post. Please try again."
-      render :edit
-    end
-  end
-
-  def destroy
-    @post = Post.find(params[:id])
-
-    if @post.destroy
-      flash[:notice] = "#{@post.title} was deleted successfully."
-      redirect_to @post.topic
-    else
-      flash[:error] = "There was an error deleting the post."
-      render :show
-    end
-  end
-
-  private
-
-  def post_params
-    params.require(:post).permit(:title, :body)
-  end
+  
+   def update
+     @post = Post.find(params[:id])
+     @post.title = params[:post][:title]
+     @post.body = params[:post][:body]
+ 
+     if @post.save
+       flash[:notice] = "Post was updated."
+       redirect_to [@post.topic, @post]
+     else
+       flash.now[:alert] = "There was an error saving the post. Please try again."
+       render :edit
+     end
+   end
+   
+   def destroy
+     @post = Post.find(params[:id])
+ 
+ # #8
+     if @post.destroy
+       flash[:notice] = "\"#{@post.title}\" was deleted successfully."
+       redirect_to @post.topic
+     else
+       flash.now[:alert] = "There was an error deleting the post."
+       render :show
+     end
+   end
+     
+  
+  
 end
