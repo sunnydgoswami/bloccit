9f552bca125b209995fc9bd19f597bce9c61a1a1
SunnyGoswami <sunnydgoswami@gmail.com>
2017-08-15 16:49:13 +0000

    assignment 28 completed

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -1,5 +1,13 @@
- module TopicsHelper
-   def user_is_authorized_for_topics?
-        current_user && current_user.admin?
-   end
- end
+
+module TopicsHelper
+
+  def user_is_authorized_to_edit_topics?
+    current_user && current_user.admin? || current_user && current_user.moderator?
+  end
+
+  def user_is_authorized_to_create_and_delete_topics?
+    current_user && current_user.admin?
+  end
+
+
+end
